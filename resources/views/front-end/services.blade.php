7203346e2452a9f9aa8a13ebbaa32e579a5d0e74
bdnahid55 <bdnahid55@gmail.com>
2025-01-05 12:10:46 +0600

    bug fix new

--- resources/views/front-end/services.blade.php
+++ resources/views/front-end/services.blade.php
@@ -33,8 +33,7 @@
 
                     @if ($ServiceData->isNotEmpty())
                         @foreach ($ServiceData as $index => $service)
-                            <div class="mt-4 col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in"
-                                data-aos-delay="100">
+                            <div class="mt-4 col-lg-4 col-md-6" data-aos="zoom-in" data-aos-delay="100">
                                 <div class="icon-box iconbox-yellow">
                                     <div class="icon">
                                         <i>
